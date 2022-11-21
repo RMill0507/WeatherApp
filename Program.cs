a072a8605105f81d7c1d5ded367c627972fe99db
Rob <Rmilla445@gmail.com>
2022-11-21 16:11:56 -0500

    updated

--- Program.cs
+++ Program.cs
@@ -9,18 +9,31 @@ namespace WeatherApp
         static void Main(string[] args)
         {
             string key = File.ReadAllText("appsettings.json");
+            //storing JSON file in the variable key
             string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+            //Calling Jobject class and parsing "key" and useing GetValue() of 
+            //appsettings APIKey
 
             Console.WriteLine("Please enter your ZipCode");
+            //Asking the user to enter zip code and storing it in zipCode
             var zipCode = Console.ReadLine();
 
             var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIKey}";
-
+            //apiCall is the URL from PostMan. Updating the zipCode value from what the user enters.
+            //Also adding APIkey to hide my API key by using our variable APIKey
             Console.WriteLine();
 
-            Console.WriteLine($"It is currently {WeatherMapTemp.GetTemp(apiCall)} degrees F in your loacation");
+            Console.WriteLine($"The current weather for this location is:");
+            Console.WriteLine();
+            Console.WriteLine($"Current Temp: {WeatherMap.GetTemp(apiCall)} degrees F");
+            Console.WriteLine($"Feels like: {WeatherMap.GetFeelsLike(apiCall)}");
+            Console.WriteLine($"Temp Min: {WeatherMap.GetTempMin(apiCall)}");
+            Console.WriteLine($"Temp Max: {WeatherMap.GetTempMax(apiCall)}");
+            Console.WriteLine($"Humidity: {WeatherMap.GetHumidity(apiCall)}");
+
+            //Using the class WeatherMapTemp to put in the temp of the location zipcode the user chose
             
-            Console.WriteLine("If your temp is less that 40 degrees F, YOU NEED TO MOVE SOUTH");
+            Console.WriteLine($"This is the weather for the zipcode {zipCode} you entered");
 
         }
     }
---
+++ WeatherMap.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public class WeatherMap
+
+    {
+        public static double GetTemp(string apiCall)//this is a method used to get the temperature of the zipcode chosen
+        {
+            var client = new HttpClient();
+            
+            var response = client.GetStringAsync(apiCall).Result;
+            
+            var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+            //creating a variable temp by accessing our temp from the main in postman and converting it to strings
+            return temp; 
+            //satisfying out return type in the method
+        }
+        public static double GetFeelsLike(string apiCall)//this is a method used to get the feelslike temp of the zipcode chosen
+        {
+            var client = new HttpClient();
+
+            var response = client.GetStringAsync(apiCall).Result;
+
+            var feelsLike = double.Parse(JObject.Parse(response)["main"]["feels_like"].ToString());
+            //creating a variable temp by accessing our temp from the main in postman and converting it to strings
+            return feelsLike;
+            //satisfying out return type in the method
+        }
+        public static double GetTempMin(string apiCall)
+        {
+            var client = new HttpClient();
+            var response = client.GetStringAsync(apiCall).Result;
+            var tempMin = double.Parse(JObject.Parse(response)["main"]["temp_min"].ToString());
+            return tempMin;
+     
+        }
+        public static double GetTempMax(string apiCall)
+        {
+            var client = new HttpClient();
+            var response = client.GetStringAsync(apiCall).Result;
+            var tempMax = double.Parse(JObject.Parse(response)["main"]["temp_max"].ToString());
+            return tempMax;
+
+        }
+        public static double GetHumidity(string apiCall)
+        {
+            var client = new HttpClient();
+            var response = client.GetStringAsync(apiCall).Result;
+            var humidity = double.Parse(JObject.Parse(response)["main"]["humidity"].ToString());
+            return humidity;
+        }
+    }
+}
